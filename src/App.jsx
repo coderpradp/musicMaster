76b1b63daf85ebfc46238450161400c61c3dd1e5
Pradip Khadka <fnfdrifter@gmail.com>
2017-03-27 14:34:28 +0545

    Setup fetching data from spotify api

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,15 @@ class App extends Component {
 
   search() {
     console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    console.log("Fetch: ", FETCH_URL);
+
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json));
   }
 
   render() {
