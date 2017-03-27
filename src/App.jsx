43d92d1b74c2ab870c74e36130f8a8df3a3cdb7c
Pradip Khadka <fnfdrifter@gmail.com>
2017-03-27 14:12:07 +0545

    Added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,8 +23,15 @@ class App extends Component {
             <FormControl
               type="text"
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if(event.key === 'Enter') {
+                  this.search();
+                }
+              }}
             />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
