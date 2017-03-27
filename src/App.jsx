5976f0df63a2f721a7ddf512348d33280380b986
Pradip Khadka <fnfdrifter@gmail.com>
2017-03-27 20:20:31 +0545

    Creation of Gallery Component and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import Profile from './Profile';
+import Gallery from './Gallery';
 import './App.css';
 
 
@@ -18,6 +19,8 @@ class App extends Component {
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
     const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+
+    /* Fetching artist data */
     fetch(FETCH_URL, {
       method: 'GET'
     })
@@ -27,10 +30,12 @@ class App extends Component {
       this.setState({artist});
 
       FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`;
+
+      /* Fetching artist's top tracks */
       fetch(FETCH_URL, {
         method: 'GET'
       })
-      .then(response => response.join())
+      .then(response => response.json())
       .then(json => {
         const { tracks } = json;
         this.setState({tracks});
@@ -66,9 +71,9 @@ class App extends Component {
               <Profile
                 artist={this.state.artist}
               />
-              <div className="Gallery">
-                Gallery
-              </div>
+              <Gallery
+                tracks={this.state.tracks}
+              />
             </div>
           : <div></div>
         }
